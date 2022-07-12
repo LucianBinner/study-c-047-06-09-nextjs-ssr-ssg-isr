1059216337c4ab88c89b6ac3816caab0775604e8
Lucian Binner <lucian.binner@hotmail.com>
2022-07-12 20:41:56 -0300

    feat: staticSiteGeneration

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,23 @@
 import Home from '../templates/Home';
+import axios from 'axios';
+import config from '../config';
+import { mapData } from '../api/map-data';
 
-export default function HomePage() {
-  return <Home />;
+export default function HomePage({ data = null }) {
+  return <Home data={data} />;
 }
+
+export const getStaticProps = async () => {
+  let getStaticPropsReturn = {
+    props: {
+      data: [],
+    },
+  };
+  try {
+    const raw = await axios.get(config.url);
+    getStaticPropsReturn.props.data = mapData(raw.data);
+    return getStaticPropsReturn;
+  } catch (error) {
+    return getStaticPropsReturn;
+  }
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,89 +1,56 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-import { mapData } from '../../api/map-data';
-
-import { GridTwoColumns } from '../../components/GridTwoColumns';
 import { GridContent } from '../../components/GridContent';
-import { GridText } from '../../components/GridText';
 import { GridImage } from '../../components/GridImage';
+import { GridText } from '../../components/GridText';
+import { GridTwoColumns } from '../../components/GridTwoColumns';
 
-import { mockBase } from '../Base/mock';
 import { Base } from '../Base';
 import { PageNotFound } from '../PageNotFound';
-import { Loading } from '../Loading';
 
+import Head from 'next/head';
 import config from '../../config';
 
-function Home() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const load = async () => {
-      try {
-        const data = await axios.get(config.url);
-        const pageData = mapData(data.data);
-        setData(pageData[0]);
-      } catch (e) {
-        setData(undefined);
-      }
-    };
-
-    load();
-  }, []);
-
-  useEffect(() => {
-    if (data === undefined) {
-      document.title = `Página não encontrada | ${config.siteName}`;
-    }
-
-    if (data && !data.slug) {
-      document.title = `Carregando... | ${config.siteName}`;
-    }
-
-    if (data && data.title) {
-      document.title = `${data.title} | ${config.siteName}`;
-    }
-  }, [data]);
-
-  if (data === undefined) {
+function Home({ data }) {
+  if (!data || !data?.length) {
     return <PageNotFound />;
   }
 
-  if (data && !data.slug) {
-    return <Loading />;
-  }
-
-  const { menu, sections, footerHtml, slug } = data;
+  const { menu, sections, footerHtml, slug, title } = data[0];
   const { links, text, link, srcImg } = menu;
 
   return (
-    <Base
-      links={links}
-      footerHtml={footerHtml}
-      logoData={{ text, link, srcImg }}
-    >
-      {sections.map((section, index) => {
-        const { component } = section;
-        const key = `${slug}-${index}`;
+    <>
+      <Head>
+        <title>
+          {title} | {config.siteName}
+        </title>
+      </Head>
+      <Base
+        links={links}
+        footerHtml={footerHtml}
+        logoData={{ text, link, srcImg }}
+      >
+        {sections.map((section, index) => {
+          const { component } = section;
+          const key = `${slug}-${index}`;
 
-        if (component === 'section.section-two-columns') {
-          return <GridTwoColumns key={key} {...section} />;
-        }
+          if (component === 'section.section-two-columns') {
+            return <GridTwoColumns key={key} {...section} />;
+          }
 
-        if (component === 'section.section-content') {
-          return <GridContent key={key} {...section} />;
-        }
+          if (component === 'section.section-content') {
+            return <GridContent key={key} {...section} />;
+          }
 
-        if (component === 'section.section-grid-text') {
-          return <GridText key={key} {...section} />;
-        }
+          if (component === 'section.section-grid-text') {
+            return <GridText key={key} {...section} />;
+          }
 
-        if (component === 'section.section-grid-image') {
-          return <GridImage key={key} {...section} />;
-        }
-      })}
-    </Base>
+          if (component === 'section.section-grid-image') {
+            return <GridImage key={key} {...section} />;
+          }
+        })}
+      </Base>
+    </>
   );
 }
 
