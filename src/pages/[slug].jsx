6b6da9acc61e559970ee97020053fd56e52ebc65
Lucian Binner <lucian.binner@hotmail.com>
2022-07-13 17:19:10 -0300

    feat: getServerSideProps

--- src/pages/[slug].jsx
+++ src/pages/[slug].jsx
@@ -5,22 +5,10 @@ export default function PageSlug({ data }) {
   return <Home data={data} />;
 }
 
-// Por padrão sempre que queremos gerar arquivos estaticos com getStaticProps no next em rotas dinamicas temos de utilizar em conjunto o getStaticPaths
-export const getStaticPaths = async () => {
-  const paths = (await loadPages()).map((pages) => {
-    return {
-      params: {
-        slug: pages.slug,
-      },
-    };
-  });
-  return {
-    paths, // Todas os caminhos que teram sua correspondencia direcionada para este coponente
-    fallback: false, // fallback em true indica que todas as páginas que não existem correspondencia tera seu caminho direcionado para este componente, já em false só direicionará as correspondencias em que estão dentro do array de paths
-  };
-};
-
-export const getStaticProps = async (context) => {
+// getServerSideProps
+// O metodo de getServerSideProps é muito similar ao getStaticProps, pois não é necessário fazer quase que alteração alguma, a única coisa que muda é no trabalho de build final do next, pois o gatStaticProps gera páginas estáticas e o getServerSideProps necessita de servido para ficar buscando as páginas em tempo real
+// O metodo de getServerSideProps ou revalidate não utilizar-se do next export, pois ocorrerá conflitos.
+export const getServerSideProps = async (context) => {
   let data = null;
   try {
     data = await loadPages(context.params.slug);
@@ -40,3 +28,40 @@ export const getStaticProps = async (context) => {
     },
   };
 };
+
+// // GetStaticProps
+// // Por padrão sempre que queremos gerar arquivos estaticos com getStaticProps no next em rotas dinamicas temos de utilizar em conjunto o getStaticPaths
+// export const getStaticPaths = async () => {
+//   const paths = (await loadPages()).map((pages) => {
+//     return {
+//       params: {
+//         slug: pages.slug,
+//       },
+//     };
+//   });
+//   return {
+//     paths, // Todas os caminhos que teram sua correspondencia direcionada para este coponente
+//     fallback: false, // fallback em true indica que todas as páginas que não existem correspondencia tera seu caminho direcionado para este componente, já em false só direicionará as correspondencias em que estão dentro do array de paths
+//   };
+// };
+
+// export const getStaticProps = async (context) => {
+//   let data = null;
+//   try {
+//     data = await loadPages(context.params.slug);
+//   } catch (error) {
+//     data = null;
+//   }
+
+//   if (!data || !data.length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,8 @@ export default function HomePage({ data = null }) {
   return <Home data={data} />;
 }
 
-export const getStaticProps = async () => {
+// getServerSideProps
+export const getServerSideProps = async () => {
   let data = [];
   try {
     data = await loadPages('landing-page');
@@ -24,3 +25,24 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+// // GetStaticProps
+// export const getStaticProps = async () => {
+//   let data = [];
+//   try {
+//     data = await loadPages('landing-page');
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   if (!data || !data.length) {
+//     // Caso não tenha dados é exibido a página notFound
+//     return {
+//       notFound: true,
+//     };
+//   }
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
