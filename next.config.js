af2dd320103c4abf75482ea989d69339180b6f10
Lucian Binner <lucian.binner@hotmail.com>
2022-07-13 16:15:22 -0300

    feat: next link

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
